72af4e631f401f61be9ffdc47c12897e86c8155d
KritishPokharel <67674035+KritishPokharel@users.noreply.github.com>
2023-11-28 15:25:05 -0500

    Main Program Start and Input Loop Setup
    Set up main function and jump to input loop.

--- submission_code.s
+++ submission_code.s
@@ -2,4 +2,8 @@
 prompt: .asciiz "Enter an integer (N >= 25): "
 error_display: .asciiz "Illegal Number! Please write valid numbers, i.e., (N >= 25) \n"
 fib_display: .asciiz "Fibonacci Sequence:\n"
+.text
+main:
+    j input_loop
+
 
